27706748ae3cc951357721aeca83ad924db43e03
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-09-24 16:11:27 +0530

    HRM server first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const conn = require('./db/conn');
+const dotenv = require('dotenv');
+dotenv.config();
+const userRoutes = require('./router/userRouters');
+const authRoutes = require('./router/authRouters');
+const cors = require('cors');
+
+app.use(cors());
+
+//database connection
+conn();
+app.use(express.json());
+
+app.use('/',express.static(__dirname + '/../client'));
+//userRoutes
+app.use(userRoutes);
+
+//authRoutes
+app.use(authRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server running at http://localhost:${process.env.PORT}`)
+});
